57e77756cca898b214d8ef30961496c31a3995f3
Rohit Kumar <rkumar8092378845@gmail.com>
2020-07-29 11:34:25 +0530

    -> Making Groups in Notification

--- app/src/main/java/com/rohitthebest/notifications/others/Constants.kt
+++ app/src/main/java/com/rohitthebest/notifications/others/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val CHANNEL2 = "channel2"
     const val CHANNEL3 = "channel3"
     const val CHANNEL4 = "channel4"
-    const val CHANNEL5 = "channel4"
+    const val CHANNEL5 = "channel5"
     const val REQUEST_CODE = 0
     const val MESSAGE_KEY = "toastMessage"
 }
